58336f2ea9ef4b2a1f568addf1d2850569695201
Jack Morby <jmorby2@illinois.edu>
2024-09-28 19:49:27 -0400

    navbar fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// 
 import './App.css'
 
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,6 +4,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Nav.css';
 
 function NavbarFlow() {
   return (
