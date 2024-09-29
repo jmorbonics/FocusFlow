4c294a1c32ac652ae2c8abb719656229b430d103
Jack Morby <jmorby2@illinois.edu>
2024-09-28 22:48:37 -0400

    add game module

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,7 +11,7 @@ function NavbarFlow() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">FocusFlow</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import NavbarFlow from './components/Nav.jsx';
 import './index.css'
+import Game from './components/Game.jsx';
+
 
 createRoot(document.getElementById('root')).render(
+
+  
   <StrictMode>
     <NavbarFlow />
     <App />
+    {/* <Game /> */}
   </StrictMode>,
 )
