d949bb6658177d0888bbdeaca3cac880c4e4a935
sdoshi4 <shaandoshi4@gmail.com>
2024-09-29 10:43:49 -0400

    add games!!!!!

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Parse from './Parse';
 import Calibrate from './Calibrate';
 import CollectData from './CollectData';
 import GPT from './components/GPT';
+import Game from './components/Game';
 
 
 
@@ -24,6 +25,7 @@ function App() {
         <Route path="/calibrate" element={<Calibrate />} />
         <Route path="/mldata" element={<CollectData />} />
         <Route path="/gpt" element={<GPT />} />
+        <Route path="/games" element={<Game />} />
       </Routes>
     </BrowserRouter>
   );
