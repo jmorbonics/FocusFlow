0af782b64c1a257faa7a50d7cb23778f094bc484
Jack Morby <jmorby2@illinois.edu>
2024-09-29 00:20:53 -0400

    add chat-gpt capability

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,9 +16,9 @@ function NavbarFlow() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="home">Home</Nav.Link>
-            <Nav.Link href="link">Link</Nav.Link>
-			<Nav.Link href="calibrate">Calibrate</Nav.Link>
-			<Nav.Link href="mldata">Get Data</Nav.Link>
+            <Nav.Link href="gpt">GPT</Nav.Link>
+            <Nav.Link href="calibrate">Calibrate</Nav.Link>
+            <Nav.Link href="mldata">Get Data</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
