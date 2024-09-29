e72c7d8cca98f69bb371f266dda6400aeee290ad
kevinh05 <khyy4321@gmail.com>
2024-09-28 21:29:50 -0500

    kevin

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Calibrate from './Calibrate.jsx';
 import BasicExample from './Nav';
 import './index.css'
-
+import CollectData from './CollectData.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BasicExample />
-    <App />
+	<CollectData />
   </StrictMode>,
 )
